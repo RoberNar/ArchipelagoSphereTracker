2cb3e7e78c02c238dc13b47c2053e27b57dd4c5f
RoberNar <74436425+RoberNar@users.noreply.github.com>
2026-03-08 22:11:22 -0300

    Update UrlClass.cs

--- src/Bot/UrlClass.cs
+++ src/Bot/UrlClass.cs
@@ -246,9 +246,6 @@ public class UrlClass
                     }
                     finally
                     {
-                        Declare.AddedChannelId.Remove(channelId);
-                        Console.WriteLine($"Finished adding URL Channel: {newUrl} in Guild: {guildId}, Channel: {channelId}");
-                    }
 
                         Declare.AddedChannelId.Remove(channelId);
                         Console.WriteLine($"Finished adding URL Channel: {newUrl} in Guild: {guildId}, Channel: {channelId}");
